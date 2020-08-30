62b23a661155e28d83111e748769f355e9a6dba4
Jonatan Machado <jonatanmachado35@gmail.com>
2020-08-30 15:31:28 -0300

    desafio concluido

---
+++ src/repositories/TransactionsRepository.ts
@@ -0,0 +1,53 @@
+import Transaction from '../models/Transaction';
+
+interface Balance {
+  income: number;
+  outcome: number;
+  total: number;
+}
+
+interface CreateDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
+class TransactionsRepository {
+  private transactions: Transaction[];
+
+  constructor() {
+    this.transactions = [];
+  }
+
+  public all(): Transaction[] {
+    return this.transactions;
+  }
+
+  public getBalance(): Balance {
+    const income: number = this.transactions
+      .filter(transaction => transaction.type === 'income')
+      .reduce((a, b) => a + b.value, 0);
+
+    const outcome: number = this.transactions
+      .filter(transaction => transaction.type === 'outcome')
+      .reduce((a, b) => a + b.value, 0);
+
+    const total = income - outcome;
+
+    return {
+      income,
+      outcome,
+      total,
+    };
+  }
+
+  public create({ title, value, type }: CreateDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
+  }
+}
+
+export default TransactionsRepository;
